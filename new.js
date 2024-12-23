fa3c1f2af6eceb856a4528db67d96ead80bf1333
pritanjan <pritanjanbaranwal@gmail.com>
2024-12-23 16:44:15 +0530

    conflict solved now

--- new.js
+++ new.js
@@ -2,10 +2,9 @@ import { username } from "./config.json";
 console.log(username);
 console.log(password);
 
-<<<<<<< HEAD
-
+// <<<<<<< HEAD
 
 console.log("this is added by the devloper B");
-=======
-console.log("this is added by the devloper A")
->>>>>>> 650f63ef9ca69b7785d74b315474acf15466403c
+// =======
+console.log("this is added by the devloper A");
+// >>>>>>> 650f63ef9ca69b7785d74b315474acf15466403c
