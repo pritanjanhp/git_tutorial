a48e7788e879621fb3a4cabe8ce66c15b7c23194
pritanjan <pritanjanbaranwal@gmail.com>
2024-12-23 16:37:26 +0530

    conflict

--- new.js
+++ new.js
@@ -2,3 +2,6 @@ import { username } from "./config.json";
 console.log(username);
 console.log(password);
 
+
+
+console.log("this is added by the devloper B");
--- notes.js
+++ notes.js
@@ -1,27 +1,24 @@
-
-
 // git init
 // git rempte add origin link
 // git config user.name pritanjan
 // git config user.name email
 
 
-// !push 
-
+// !push
 // git add .
 // git commit -m 'first commit'
 // git push origin master
 
+
 // ! pull
-
 // git pull origin master
-
 
 // ! clone
 // git clone (url which we want to clone)
 
 
 // ? Branches
+
 
 // ! create new branch
 // git checkout -b 'new_branch'
@@ -35,5 +32,8 @@
 // ! Delete branch
 // git branch -d 'branch_name'
 
-
+// ! use code after adding the file name in .gitignore file
+// git rm -r --cached
+// git add .
+// git commit -m 'msg'
 
