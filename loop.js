83f39bba9a1962ffb4947d27a7b17142e3660c43
pritanjan <pritanjanbaranwal@gmail.com>
2024-12-23 13:54:41 +0530

    third commit

--- loop.js
+++ loop.js
@@ -3,3 +3,4 @@ var arr = [1, 2, 3, 4];
 for (let i in arr) {
   console.log(arr[i]);
 }
+
---
+++ note.js
@@ -0,0 +1,20 @@
+// !push 
+
+// git add.
+// git commit -m 'commit'
+// git push origin master
+
+
+// ! pull
+// git pull origin master
+
+
+// ! create to new branch
+// git checkout -b 'new_branch'
+
+// ! switch to new branch
+// git checkout -b 'new_branch'
+
+// ! delete branch
+// git branch -d 'branch_name'
+
